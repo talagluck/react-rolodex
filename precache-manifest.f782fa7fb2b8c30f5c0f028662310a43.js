f65f9c3e1c33352e37919054cdec4221c4b15ec0
Tal Gluck <michaellgl@gmail.com>
2020-01-24 21:23:54 -0500

    Updates

---
+++ precache-manifest.f782fa7fb2b8c30f5c0f028662310a43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8e363169191620c2d5cb5b52e2eb9a9",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "a02000e833a277d9bdc2",
+    "url": "/react-rolodex/static/css/main.6aaa44f4.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/react-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a02000e833a277d9bdc2",
+    "url": "/react-rolodex/static/js/main.b09fc8bc.chunk.js"
+  },
+  {
+    "revision": "ccbf070b2ecd5be66d8a",
+    "url": "/react-rolodex/static/js/runtime-main.97754a52.js"
+  }
+]);
